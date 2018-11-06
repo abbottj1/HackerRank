2f39016975520ffaf66209108f1655b5032dfac7
Josh Abbott <josh.abbott@live.com>
2018-11-06 07:59:53 +0000

    https://www.hackerrank.com/challenges/jumping-on-the-clouds

---
+++ HackerRank/Preparation/JumpingOnTheClouds.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] c_temp = Console.ReadLine().Split(' ');
+        int[] c = Array.ConvertAll(c_temp,Int32.Parse);
+        
+        int pos = 0;
+        int jumps = 0;
+        
+        while(pos < n) {
+            if(pos+2 < n && c[pos+2]==0){
+                pos += 2;
+            } else {
+                pos ++;
+            }
+            jumps++;
+            
+            if(pos == n - 1) {
+                break;
+            }
+        }
+        
+        Console.WriteLine(jumps);
+    }
+}
