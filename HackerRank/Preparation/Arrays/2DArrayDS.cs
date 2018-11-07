fda449274c1c846e37aa9322a7770812a202edc1
Josh Abbott <josh.abbott@live.com>
2018-11-07 07:52:36 +0000

    https://www.hackerrank.com/challenges/2d-array/

---
+++ HackerRank/Preparation/Arrays/2DArrayDS.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+        // Lowest possible if all are -9
+        int maxSum = -63;
+        
+        for(int i = 1; i < arr.Length - 1; i++)
+        {
+            for(int j = 1; j < arr[i].Length - 1; j++)
+            {
+                int sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+                
+                if(sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+        }
+        
+        return maxSum;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
