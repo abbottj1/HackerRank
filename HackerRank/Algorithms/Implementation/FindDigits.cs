b0fd28d821dd9457872cffbf5a3f26cdfb605152
Josh Abbott <josh.abbott@live.com>
2019-01-19 18:54:24 +0000

    https://www.hackerrank.com/challenges/find-digits/

---
+++ HackerRank/Algorithms/Implementation/FindDigits.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        var divisors = 0;
+        var characters = n.ToString().ToCharArray();
+
+        foreach(var character in characters)
+        {
+            var num = Char.GetNumericValue(character);
+
+            if(n%num == 0)
+            {
+                divisors++;
+            }
+        }
+
+        return divisors;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = findDigits(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
